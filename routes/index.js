a213b959cc90b22bd95b6b007b4152868a85f858
German Aguirre <German Aguirre>
2016-11-25 01:56:37 -0500

    AgroSolidariaAlpha

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var router = express.Router();
+
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'agroesperanza'
+  });
+
+  connection.connect();
+
+  /* GET home page. */
+
+  router.get('/', function (req, res, next) {
+    res.render('index', { title: 'LOGIN' });
+  });
+
+
+  router.post('/', function (req, res, next) {
+    console.log('Lo que hay', req.params);
+    console.log('body', req.body);
+    connection.query('Select * from login where User = "' + req.body.usuario + '" and Password ="' + req.body.password + '";', function (err, rows, fields) {
+      if (!err) {
+        res.render('Menu', {
+         title: 'Exito'
+        });
+      } else {
+        res.json({
+          message: 'Error'
+        });
+      }
+    });
+  });
+
+
+module.exports = router;
